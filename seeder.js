44d275c9d0b4eb096d8d4e8d680f82b08340bb92
Fatih <h.fatih_@outlook.com>
2022-08-04 19:39:55 +0300

    related model and new seed

--- seeder.js
+++ seeder.js
@@ -7,18 +7,21 @@ dotenv.config({ path: './config/config.env' })
 
 // Load models
 const Bootcamp = require('./models/Bootcamps')
+const Course = require('./models/Course')
 
 // Connect to db
 mongoose.connect(process.env.MONGO_URI);
 
 // Read JSON files
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'))
+const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'))
 
 // Import into db
 // node seeder -i
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps)
+        await Course.create(courses)
         console.log("Data imported as seed mode.".green.inverse)
         process.exit()
     } catch (error) {
@@ -31,6 +34,7 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Bootcamp.deleteMany()
+        await Course.deleteMany()
         console.log("Data destroyed.".red.inverse)
         process.exit()
     } catch (error) {
